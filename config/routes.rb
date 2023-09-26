ea803c0208da269fecbe746696fd4b63485b8e62
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-09-26 16:57:05 +0000

    Incorporate route for square roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  
+  #get("/", controller: "home", action: "home_action")
+
+  get("/square/new", controller: "square", action: "square_new")
+
+  get("/square/results", controller: "square", action: "square_results")
+
+  get("/square_root/new", controller: "square_root", action: "square_root_new")
+
+  get("/square_root/results", controller: "square_root", action: "square_root_results")
+
+  # get("/payment/new")
+
+  # get("/payment/results")
+
+  # get("/random/new")
+
+  # get("/random/results")
 end
