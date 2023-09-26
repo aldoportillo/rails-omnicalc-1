3cb2dfbe93e15ae2382ade48d7a1df1fa71be215
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-09-26 16:53:11 +0000

    Fix name

--- app/controllers/square_root_controller.rb
+++ app/controllers/square_root_controller.rb
@@ -1,10 +1,10 @@
 class SquareController < ApplicationController
 
-  def square_new
+  def square_root_new
     render({:template => "/form_templates/square_root"})
   end
 
-  def square_results
+  def square_root_results
     @user_number = params.fetch("user_number")
     @result = @user_number.to_f ** 0.5
 
